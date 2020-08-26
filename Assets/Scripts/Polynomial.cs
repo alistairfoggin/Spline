fd99d9170105ac91933b8cea41b7db8b49561666
Alistair Foggin <alistair.foggin02@gmail.com>
2020-08-26 23:22:07 +0100

    Spline working with manual slopes

---
+++ Assets/Scripts/Polynomial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polynomial
+{
+    public float[] Coefficients { get; set; }
+
+    public Polynomial()
+    {
+        Coefficients = new float[0];
+        Coefficients[0] = 0.0f;
+    }
+
+    // Coefficients are an array that correspond as follows: a + bx + cx^2 + dx^3 ...
+    public Polynomial(float[] coefficients)
+    {
+        Coefficients = coefficients;
+    }
+
+    public float calculateValue(float x)
+    {
+        float result = 0;
+        for (int i = 0; i < Coefficients.Length; i++)
+        {
+            result += Coefficients[i] * Mathf.Pow(x, i);
+        }
+        return result;
+    }
+
+    public Polynomial calculateDerivative()
+    {
+        float[] coeffs = new float[Coefficients.Length - 1];
+        for (int i = 0; i < coeffs.Length; i++)
+        {
+            float coefficient = Coefficients[i + 1];
+            coefficient *= (i + 1);
+            coeffs[i] = coefficient;
+        }
+
+        return new Polynomial(coeffs);
+    }
+}
---
+++ Assets/Scripts/Spline.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Spline : MonoBehaviour
+{
+    public Vector3[] points = new Vector3[0];
+    public Vector3[] slopes = new Vector3[0];
+    public int subdivisions = 5;
+
+    private LineRenderer lineRenderer;
+    private Vector3[] subPoints;
+
+    private void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        subPoints = new Vector3[(points.Length - 1) * subdivisions];
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Polynomial fx = new Polynomial(CalcCoeffsForAxis(0, i));
+            Polynomial fy = new Polynomial(CalcCoeffsForAxis(1, i));
+            Polynomial fz = new Polynomial(CalcCoeffsForAxis(2, i));
+
+            for (int j = 0; j < subdivisions; j++)
+            {
+                float t = (float)j / (float)subdivisions;
+                float x = fx.calculateValue(t);
+                float y = fy.calculateValue(t);
+                float z = fz.calculateValue(t);
+                subPoints[i * subdivisions + j] = new Vector3(x, y, z);
+            }
+        }
+        subPoints[subPoints.Length - 1] = points[points.Length - 1];
+
+        lineRenderer.positionCount = subPoints.Length;
+        lineRenderer.SetPositions(AdjustPointsForPosition(subPoints));
+    }
+
+
+    // axis is 0, 1, or 2 corresponding with x, y, and z
+    float[] CalcCoeffsForAxis(int axis, int index)
+    {
+        // x = a + bt + ct^2 + dt^3
+        // s = b + 2ct + 3dt^2
+        float[] coefficients = new float[4];
+        float x0 = points[index][axis];
+        float x1 = points[index + 1][axis];
+        float s0 = slopes[index][axis];
+        float s1 = slopes[index + 1][axis];
+
+        coefficients[0] = x0;
+        coefficients[1] = s0;
+        coefficients[2] = 3 * (x1 - x0) - 2 * s0 - s1;
+        coefficients[3] = 2 * (x0 - x1) + s0 + s1;
+        return coefficients;
+    }
+
+    private Vector3[] AdjustPointsForPosition(Vector3[] thesePoints)
+    {
+        Vector3[] newPoints = new Vector3[thesePoints.Length];
+
+        for (int i = 0; i < thesePoints.Length; i++)
+        {
+            float x = transform.position.x + thesePoints[i].x;
+            float y = transform.position.y + thesePoints[i].y;
+            float z = transform.position.z + thesePoints[i].z;
+            newPoints[i] = new Vector3(x, y, z);
+        }
+
+        return newPoints;
+    }
+}
